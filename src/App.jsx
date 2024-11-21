80d7676b4a597be1b675d3d12aab36d909bfd40c
Your Name <you@example.com>
2024-11-21 12:55:37 +0200

    button style

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
-// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home/Home';
 
 
 function App() {
@@ -7,19 +8,12 @@ function App() {
   return (
     <div className="app">
         <div className="appContainer">
-          <h1 className="text-4xl font-bold text-blue-500">  قران  كريم</h1>
-          {/* <Router>
+          <Router>
             <Routes>
-              <Route  path="/"  element={<Home/>}/>
-              <Route  path="/courses"  element={<Courses/>}/>
-              <Route  path="/courses"  element={<page/>}/>
-              <Route  path="/...."  element={<..../>}/>
-              <Route>
-                <Route  path="....."  element={<..../>}/>
-                <Route  path="....."  element={<..../>}/>
-              </Route>
+            <Route path='/'  element={<Home/>}/>
             </Routes>
-          </Router> */}
+          </Router>
+          <button className='globalButton'> حجز حصة تجريبية</button>
         </div>
     </div>
   )
