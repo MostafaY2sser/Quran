05e5169b0e19c29087e6bd5cb1a25b4cddb5de40
Your Name <you@example.com>
2024-12-03 16:11:44 +0200

    mostafa

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,15 +1,15 @@
 
 
-import Amr from '../../Components/Amr/Amr'
-import BlogsSection from '../../Components/BlogsSection/BlogsSection'
-import CoursesSection from '../../Components/CoursesSection/CoursesSection'
-import EgazatSection from '../../Components/Egazat/EgazatSection'
+// import Amr from '../../Components/Amr/Amr'
+// import BlogsSection from '../../Components/BlogsSection/BlogsSection'
+// import CoursesSection from '../../Components/CoursesSection/CoursesSection'
+// import EgazatSection from '../../Components/Egazat/EgazatSection'
 import HeroHome from '../../Components/HeroHome/HeroHome'
-import Numbers from '../../Components/Numbers/Numbers'
-import Opinions from '../../Components/Opinions/Opinions'
-import PlanBG from '../../Components/Plan/PlanBG'
-import StartLearn from '../../Components/StartLearn/StartLearn'
-import WhyUs from '../../Components/WhyUs/WhyUs'
+// import Numbers from '../../Components/Numbers/Numbers'
+// import Opinions from '../../Components/Opinions/Opinions'
+// import PlanBG from '../../Components/Plan/PlanBG'
+// import StartLearn from '../../Components/StartLearn/StartLearn'
+// import WhyUs from '../../Components/WhyUs/WhyUs'
 import './home.css'
 
 
@@ -21,7 +21,7 @@ const Home = () => {
     return (
         <div className="home">
             <HeroHome/>
-            <Amr/>
+            {/* <Amr/>
             <WhyUs/>
             <StartLearn/>
             <PlanBG/>
@@ -29,7 +29,7 @@ const Home = () => {
             <CoursesSection/>
             <Opinions/>
             <EgazatSection/>
-            <BlogsSection/>
+            <BlogsSection/> */}
         </div>
     )
 }
